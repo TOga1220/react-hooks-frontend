3a7a3eb78eb480a06f05f388563c90892849b310
ogawa <birdchild84@gmail.com>
2023-03-04 18:45:31 +0900

    useState基礎

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Basic1 = (props) => {
     const clickHandler = () => {
         console.log("clicked")
     }
 
+    // const [count, setCount] = useState(0)
+    const [product, setProduct] = useState({name:"", price:""})
     return (
     <div>
-        <button onClick={clickHandler}>Button
-        </button>
-        <h1>Hello {props.name}</h1>
+        {/* <button onClick={() => {setCount(previousCount=>previousCount+1); setCount(previousCount=>previousCount+1) } }>
+            Count {count}
+        </button> */}
+        <form>
+            <input type="text" value={product.name}
+            onChange={evt => setProduct({...product, name: evt.target.value})}/>
+
+            <input type="text" value={product.price}
+            onChange={evt => setProduct({...product, price: evt.target.value})}/>
+
+        </form>
+        <h3>Product name is {product.name}</h3>
+        <h3>Product price is {product.price}</h3>
+
     </div>
     )
 }
